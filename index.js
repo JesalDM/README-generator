717e75c728c902f6a6fe1ed2efa1099f71d29c53
Jesal Mehta <jesaldmehta@gmail.com>
2020-11-18 21:34:29 -0500

    Installed the inquirer package and imported it to index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// importing the inquirer package
+import inquirer from "inquirer";
+
+// array of questions for user
+const questions = [
+    
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();

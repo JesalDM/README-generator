71aea8eb1cbf19b75a4943c18065c3c5bd17513b
Jesal Mehta <jesaldmehta@gmail.com>
2020-11-18 23:37:38 -0500

    Added an array of objects containing questions that will be prompted to the user to get their input in index.js

--- index.js
+++ index.js
@@ -3,8 +3,56 @@ import inquirer from "inquirer";
 
 // array of questions for user
 const questions = [
-    
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the instructions to use your application?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What are the guidelines/covenants for contributing to your application?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the instructions for running tests on your application?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Under which license is the application covered?",
+        choices: ['MIT', 'ISC', 'Apache', 'GPL', 'BSD'],
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+
 ];
+
+inquirer.prompt(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {

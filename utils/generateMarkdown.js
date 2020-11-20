7b87c543c00a565c5e9206f0c8e96f165c1df19b
Jesal Mehta <jesaldmehta@gmail.com>
2020-11-20 00:57:27 -0500

    Added code to include the license badge in the readme template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,7 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 return `# ${data.title}
+[![License:${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})
 
 ## Description
 ${data.description}
